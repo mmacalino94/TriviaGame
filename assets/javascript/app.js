92313434ab7bd3c7cd558d53c0aad2a8b985e5e1
White Lily <mmacalino03@gmail.com>
2018-07-06 15:06:13 -0400

    Trivia Q&A entered in java

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+var triviaQuestions = [{
+	question: "What is thought to be the very fist video game?",
+	answerList: ["Tetris", "Bertie the Brain", "Pong", "Galaxy Game"],
+	answer: 2
+},{
+	question: "Which video game is the best-selling platform game of all time?",
+	answerList: ["Call of Duty", "Tetris", "Super Mario Bros.", "Minecraft"],
+	answer: 1
+},{
+	question: "What inspired the creation of Pac-Man",
+	answerList: ["Pie", "Cake", "Turkey Stuffing", "Pizza"],
+	answer: 3
+},{
+	question: "How did the concept of the Pokemon universe come in to fruition?",
+	answerList: ["Card Collecting", "Dog Breeding", "Insect Collecting", "Petting Zoo"],
+	answer: 2
+},{
+	question: "Which one listed below is Sony's unofficial tech mascot for PlayStation?",
+	answerList: ["Rubber Duckie", "Dunny Rabbit", "Spyro", "Red Ball"],
+	answer: 0
+},{
+	question: "How many sequels does Pixar currently have released? (as of August 2016)",
+	answerList: ["5", "3", "6", "7"],
+	answer: 0
+},{
+	question: "Who is Princess Zelda named after",
+	answerList: ["Zelda Williams", "Zelda Fitzgerald", "Zelda Spellman", "Zelda Wynn Valdes"],
+	answer: 1
+},{
+	question: "Which celebrity named their child after a character in The Legend of Zelda?",
+	answerList: ["Dwayne 'The Rock' Johnson", "Eminem", "Robin Williams", "Jack Black"],
+	answer: 2
+},{
+	question: "Which pro baseball team did Nintendo become majority owner of in 1992?",
+	answerList: ["Oakland Athletics", "Seattle Mariners", "Kansas City Royals", "Tampa Bay Rays"],
+	answer: 1
+},{
+	question: "What was Nintendo's original line of business when the company was founded in 1889?",
+	answerList: ["Toy Figures", "Marbles", "Paper", "Playing Cards"],
+	answer: 3
+},{
+	question: "Which video game saved the company, Square Enix, from bankruptcy?",
+	answerList: ["Dragon Quest", "Kingdom Hearts", "Final Fantasy", "Tomb Raider"],
+	answer: 2
+},{
+	question: "Which one was the first video game based movie?",
+	answerList: ["Tomb Raider", "Super Mario Bros", "Resident Evil", "Street Fighter"],
+	answer: 1
+},{
+	question: "Which video game company did Disney team up with to create Kingdom Hearts?",
+	answerList: ["Nintendo", "Ubisoft", "Sega", "Square Enix"],
+	answer: 3
+},{
+	question: "Which PC game was delayed in the release because of a hidden picture of the developer's buttocks?",
+	answerList: ["Halo 2", "Fortnite", "Minecraft", "Overwatch"],
+	answer: 0
+},{
+	question: "Which planet size is approximately the same as Minecraft?",
+	answerList: ["Jupiter", "Earth", "Neptune", "Uranus"],
+	answer: 2
+}];
